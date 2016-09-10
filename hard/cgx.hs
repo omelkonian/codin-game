aee689da0a86f3f3dcd0044592b0afcfd2f913ea
Orestis Melkonian <melkon.or@gmail.com>
2016-09-10 15:56:17 +0200

    CGX: parser complete

--- hard/cgx.hs
+++ hard/cgx.hs
@@ -11,63 +11,74 @@ import qualified Text.PrettyPrint as PP
 
 
 -- Data types
-data Primitive = Boolean Bool
-               | Str String
-               | Number String
+data Primitive = B Bool
+               | S String
+               | N String
                deriving(Show, Eq)
 
--- type Block = [Element]
+data KeyValue = KVP String Primitive
+              | KVB String Block
+              deriving(Show, Eq)
 
--- data KeyValue = KeyValueBlock String Block
---               | KeyValuePrimitive String Primitive
+data Block = BL [Element]
+           deriving(Show, Eq)
 
--- data Element = Block
---              | KeyValue
---              | Primitive
+data Element = EK KeyValue
+             | EP Primitive
+             | EB Block
+             deriving(Show, Eq)
 
--- elementP :: Parser Element
--- elementP = blockP <|> keyValueP <|> primitiveP
+------------------ Element -----------------------
+elementP :: Parser Element
+elementP = lexeme $
+    try (EK <$> keyValueP)
+    <|> (EP <$> primitiveP)
+    <|> (EB <$> blockP)
 
--- blockP :: Parser Block
--- blockP = do
---     char '('
---     el <- many1 elementP
---     char ')'
---     return  el
+------------------- Block ------------------------
+blockP :: Parser Block
+blockP = lexeme $
+  char '(' *> (BL <$> blockElementsP) <* char ')'
 
--- keyValueP :: Parser KeyValue
--- keyValueP = keyValueBlockP <|> keyValuePrimP
+blockElementsP :: Parser [Element]
+blockElementsP =
+  lexeme $ (elementP `sepBy` (lexeme $ char ';'))
 
--- keyValueBlockP :: Parser KeyValue
--- keyValueBlockP = do
---     s <- stringP
---     char '='
---     b <- blockP
---     return $ KeyValueBlock s b
+------------------ Key Value ---------------------
+keyValueP :: Parser KeyValue
+keyValueP = lexeme $
+  keyValuePrimP <|> keyValueBlockP
 
--- keyValuePrimP :: Parser KeyValue
--- keyValuePrimP = do
---     s <- stringP
---     char '='
---     p <- primitiveP
---     return $ KeyValuePrimitive s p
+keyValuePrimP :: Parser KeyValue
+keyValuePrimP = do
+    s <- stringP
+    char '='
+    p <- primitiveP
+    return $ KVP s p
 
--- Parsers
+keyValueBlockP :: Parser KeyValue
+keyValueBlockP = do
+    s <- stringP
+    char '='
+    b <- blockP
+    return $ KVB s b
+
+------------------ Primitive ---------------------
 primitiveP :: Parser Primitive
-primitiveP =
-    (Str <$> stringP) <|>
-    (Boolean <$> booleanP) <|>
-    (Number <$> numberP)
+primitiveP = lexeme $
+    (S <$> stringP)  <|>
+    (B <$> booleanP) <|>
+    (N <$> numberP)
 
 stringP :: Parser String
 stringP =
     char '\'' *> (many $ noneOf ['\'']) <* char '\''
 
 numberP :: Parser String
-numberP = many1 digit
+numberP = lexeme (many1 digit)
 
 booleanP :: Parser Bool
-booleanP = lexeme $ booleanTrue <|> booleanFalse
+booleanP = booleanTrue <|> booleanFalse
 
 booleanTrue :: Parser Bool
 booleanTrue = string "true" *> pure True
@@ -75,15 +86,16 @@ booleanTrue = string "true" *> pure True
 booleanFalse :: Parser Bool
 booleanFalse = string "false" *> pure False
 
+------------------ Whitespace --------------------
 lexeme p = ws *> p <* ws
 
 ws :: Parser String
 ws = many $ oneOf " \t\n"
 
-
 -- Main
 main :: IO ()
 main = forever $ do
+    hSetBuffering stdin LineBuffering
     cgxline <- getLine
-    parseTest primitiveP cgxline
+    parseTest elementP cgxline
     return ()
