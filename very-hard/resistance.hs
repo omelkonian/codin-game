a7156049795db781ba33af80e415a5851f36713f
Orestis Melkonian <melkon.or@gmail.com>
2016-09-15 21:29:17 +0200

    resistance: almost there...

--- very-hard/resistance.hs
+++ very-hard/resistance.hs
@@ -1,24 +1,27 @@
-import Data.Set (Set, fromList, member)
+import qualified Data.Map as Map
+import Data.List (inits, tails)
+import Data.Map (Map)
 import Data.Array (listArray, (!))
 import Data.Ix (range)
+import Data.Maybe (fromMaybe)
 import Control.Monad (replicateM)
+import Debug.Trace (trace)
 
 type MorseCode = String
-type Dictionary = Set String
+type Dictionary = Map MorseCode Int
+type Subsequences = Map (Int, Int) String
 
 main :: IO ()
 main = do
-    input_line <- getLine
-    let morse = input_line :: String
+    morse <- getLine
     input_line <- getLine
     let n = read input_line :: Int
-
     ws <- replicateM n getLine
     let res = getNumber morse $ wordsToDict ws
     print res
 
 getNumber :: MorseCode -> Dictionary -> Int
-getNumber morse dict = go len
+getNumber morse dict = go len -- -1??
   where len = length morse
 
         go (-1) = 0
@@ -26,20 +29,39 @@ getNumber morse dict = go len
         go n = sum [calc n k | k <- [n,n-1..0]]
 
         calc index subsize =
-          if member (substring subsize index morse) dict then
-            arr ! (subsize - 1)
-          else
-            0
+          if k /= 0 then k * arr ! (subsize - 1) else 0
+          where s = substring subsize index morse
+                k = count s dict
+
+        -- calc index subsize = trace (show index ++ ":" ++ show subsize) $
+        --   case fetch_dict of
+        --     Just k -> count k dict * fetch_array
+        --     Nothing -> 0
+        --   where fetch_dict = Map.lookup (subsize, index) subs
+        --         fetch_array = arr ! (subsize - 1)
 
         bounds = (-1, len)
         arr = listArray bounds [go x | x <- range bounds]
-        -- subs = listArray bounds []
+        subs = trace (show $ subseq morse) subseq morse
 
 substring :: Int -> Int -> String -> String
 substring start end str = take (end - start + 1) $ drop (start - 1) str
 
+continuousSubsequences :: [a] -> [[a]]
+continuousSubsequences = filter (not . null) . concatMap tails . inits
+
+subseq :: String -> Subsequences
+subseq xs = Map.fromList $ map (\(i, str) -> ((head i, last i), str)) $ zip indices s
+  where s = continuousSubsequences xs
+        indices = continuousSubsequences [0..]
+
+count :: MorseCode -> Dictionary -> Int
+count morse dict = fromMaybe 0 (Map.lookup morse dict)
+
 wordsToDict :: [String] -> Dictionary
-wordsToDict = fromList . map wordToMorse
+wordsToDict ws = dict
+  where ms = map wordToMorse ws :: [MorseCode]
+        dict = Map.fromListWith (+) [(m, 1) | m <- ms] :: Dictionary
 
 wordToMorse :: String -> MorseCode
 wordToMorse = foldr ((++) . charToMorse) ""
