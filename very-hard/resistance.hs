628bfed02057d2cacc3a784b7f18cbe61465e578
Orestis Melkonian <melkon.or@gmail.com>
2016-09-18 13:11:16 +0200

    resistance: solved

--- very-hard/resistance.hs
+++ very-hard/resistance.hs
@@ -1,15 +1,12 @@
 import qualified Data.Map as Map
-import Data.List (inits, tails)
 import Data.Map (Map)
 import Data.Array (listArray, (!))
 import Data.Ix (range)
 import Data.Maybe (fromMaybe)
 import Control.Monad (replicateM)
-import Debug.Trace (trace)
 
 type MorseCode = String
 type Dictionary = Map MorseCode Int
-type Subsequences = Map (Int, Int) String
 
 main :: IO ()
 main = do
@@ -21,39 +18,24 @@ main = do
     print res
 
 getNumber :: MorseCode -> Dictionary -> Int
-getNumber morse dict = go len -- -1??
+getNumber morse dict = go len
   where len = length morse
 
         go (-1) = 0
         go 0 = 1
-        go n = sum [calc n k | k <- [n,n-1..0]]
+        go n = sum [calc n k | k <- [n,n-1..smallest]]
+          where smallest = max 0 (n-80)
 
         calc index subsize =
           if k /= 0 then k * arr ! (subsize - 1) else 0
           where s = substring subsize index morse
                 k = count s dict
 
-        -- calc index subsize = trace (show index ++ ":" ++ show subsize) $
-        --   case fetch_dict of
-        --     Just k -> count k dict * fetch_array
-        --     Nothing -> 0
-        --   where fetch_dict = Map.lookup (subsize, index) subs
-        --         fetch_array = arr ! (subsize - 1)
-
         bounds = (-1, len)
         arr = listArray bounds [go x | x <- range bounds]
-        subs = trace (show $ subseq morse) subseq morse
 
 substring :: Int -> Int -> String -> String
 substring start end str = take (end - start + 1) $ drop (start - 1) str
-
-continuousSubsequences :: [a] -> [[a]]
-continuousSubsequences = filter (not . null) . concatMap tails . inits
-
-subseq :: String -> Subsequences
-subseq xs = Map.fromList $ map (\(i, str) -> ((head i, last i), str)) $ zip indices s
-  where s = continuousSubsequences xs
-        indices = continuousSubsequences [0..]
 
 count :: MorseCode -> Dictionary -> Int
 count morse dict = fromMaybe 0 (Map.lookup morse dict)
